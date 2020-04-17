f8408cf002700c10ee6a28da9185b5c8f388ed9f
Mahbubur Riad <mahbubur.riad@gmail.com>
2020-04-18 02:19:01 +0600

    add springbooApplication for enabling cron job

--- src/main/java/mahbuburriad/covid19tracker/Covid19TrackerApplication.java
+++ src/main/java/mahbuburriad/covid19tracker/Covid19TrackerApplication.java
@@ -2,8 +2,11 @@ package mahbuburriad.covid19tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+//enable the scheduling for cron job
+@EnableScheduling
 public class Covid19TrackerApplication {
 
 	public static void main(String[] args) {
