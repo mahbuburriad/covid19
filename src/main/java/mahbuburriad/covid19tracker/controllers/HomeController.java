d9be5c8c3126942476c5fd2cd26d13825d36f672
Mahbubur Riad <mahbubur.riad@gmail.com>
2020-04-18 22:56:59 +0600

    added comment to every lmodules

--- src/main/java/mahbuburriad/covid19tracker/controllers/HomeController.java
+++ src/main/java/mahbuburriad/covid19tracker/controllers/HomeController.java
@@ -17,28 +17,46 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model){
+        //for recovered section
         List<LocationStats> rStats = data.getrStats();
+        //sum of all recovered data
         int totalRecoveredStats = rStats.stream().mapToInt(rStat-> rStat.getLatestRecoveredCases()).sum();
+        //sum of all previous recovered data
         int prevRecoveredStas = rStats.stream().mapToInt(rStat -> rStat.getDiffFromPrevRecovered()).sum();
+        //initialize the recovered stats as locationStats
         model.addAttribute("locationStats", rStats);
+        //pass totalRecovered states data to totalRecoveredStats
         model.addAttribute("totalRecoveredStats", totalRecoveredStats);
+        //pass previous recovered stats data to prevRecoveredStats
         model.addAttribute("prevRecoveredStas", prevRecoveredStas);
 
+        //for death section
         List<LocationStats> dStats = data.getdStats();
+        //sum of all death data
         int totalDeathStats = dStats.stream().mapToInt(dStat -> dStat.getLastestDeathCases()).sum();
+        //sum of all previous recovered data
         int prevDeathStats = dStats.stream().mapToInt(dStat -> dStat.getDiffFromPrevDeath()).sum();
+        //initialize the recovered stats as locationStats
         model.addAttribute("locationStats", dStats);
+        //pass total Death Stats as totalDeathStats
         model.addAttribute("totalDeathStats", totalDeathStats);
+        //pass previous death stats as prevDeathStats
         model.addAttribute("prevDeathStats", prevDeathStats);
 
-
+        //for confirm section
         List<LocationStats> stats = data.getStats();
+        //sum of all confirmed data
         int totalReportedCases = stats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        //sum of all previous confirmed data
         int totalNewCases = stats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        //initialize the confirm stats as locationStats
         model.addAttribute("locationStats", stats);
+        //pass total confirmed data as totalReportedCases
         model.addAttribute("totalReportedCases", totalReportedCases);
+        //pass previous confirmed case as totalNewCases
         model.addAttribute("totalNewCases", totalNewCases);
 
+        //return value to index.html webPage
         return "index";
     }
 }
