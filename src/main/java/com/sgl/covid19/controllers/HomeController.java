c700fa4d740bfc1c8527abfdad88c2c1bd986333
mahbuburriad <mahbubur.riad@northsouth.edu>
2020-05-14 23:53:36 +0600

    design chagne

--- src/main/java/com/sgl/covid19/controllers/HomeController.java
+++ src/main/java/com/sgl/covid19/controllers/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import java.util.List;
 import java.text.NumberFormat;
 
@@ -16,17 +17,17 @@ public class HomeController {
     @Autowired
     Data data;
 
-      @GetMapping("/")
+    @GetMapping("/")
 
-    public String home(Model model){
+    public String home(Model model) {
 
-          /**
-           * This data is for Recovery stats
-           */
-          //for recovered section
+        /**
+         * This data is for Recovery stats
+         */
+        //for recovered section
         List<LocationStats> rStats = data.getrStats();
         //sum of all recovered data
-        int totalRecoveredStat = rStats.stream().mapToInt(rStat-> rStat.getLatestRecoveredCases()).sum();
+        int totalRecoveredStat = rStats.stream().mapToInt(rStat -> rStat.getLatestRecoveredCases()).sum();
         String totalRecoveredStats = NumberFormat.getIntegerInstance().format(totalRecoveredStat);
         //sum of all previous recovered data
         int prevRecoveredStat = rStats.stream().mapToInt(rStat -> rStat.getDiffFromPrevRecovered()).sum();
@@ -38,10 +39,10 @@ public class HomeController {
         //pass previous recovered stats data to prevRecoveredStats
         model.addAttribute("prevRecoveredStas", prevRecoveredStas);
 
-          /**
-           * This data is for death stats
-           */
-          //for death section
+        /**
+         * This data is for death stats
+         */
+        //for death section
         List<LocationStats> dStats = data.getdStats();
         //sum of all death data
         int totalDeathStat = dStats.stream().mapToInt(dStat -> dStat.getLastestDeathCases()).sum();
@@ -56,10 +57,10 @@ public class HomeController {
         //pass previous death stats as prevDeathStats
         model.addAttribute("prevDeathStats", prevDeathStats);
 
-          /**
-           * This data is for confirm stats
-           */
-          //for confirm section
+        /**
+         * This data is for confirm stats
+         */
+        //for confirm section
         List<LocationStats> stats = data.getStats();
         //sum of all confirmed data
         int totalReportedCase = stats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
@@ -68,12 +69,12 @@ public class HomeController {
         int totalNewCase = stats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
         String totalNewCases = NumberFormat.getIntegerInstance().format(totalNewCase);
 
-          /**
-           * find max number
-           */
-          //int array = stats.stream().mapToInt(stat -> stat.getLatestTotalCases()).max().orElseThrow(NoSuchElementException::new);
+        /**
+         * find max number
+         */
+        //int array = stats.stream().mapToInt(stat -> stat.getLatestTotalCases()).max().orElseThrow(NoSuchElementException::new);
 
-          //initialize the confirm stats as locationStats
+        //initialize the confirm stats as locationStats
         model.addAttribute("locationStats", stats);
         //pass total confirmed data as totalReportedCases
         model.addAttribute("totalReportedCases", totalReportedCases);
@@ -82,37 +83,37 @@ public class HomeController {
         //model.addAttribute("array", array);
 
 
-          /**
-           * Active Cases
-           */
-          int activeCase = totalReportedCase - (totalDeathStat+totalRecoveredStat);
-          String activeCases = NumberFormat.getIntegerInstance().format(activeCase);
-          model.addAttribute("activeCases", activeCases);
+        /**
+         * Active Cases
+         */
+        int activeCase = totalReportedCase - (totalDeathStat + totalRecoveredStat);
+        String activeCases = NumberFormat.getIntegerInstance().format(activeCase);
+        model.addAttribute("activeCases", activeCases);
 
-          /**
-           * last seven days record
-           */
-          int lastSevenDaysRecords = stats.stream().mapToInt(rStat -> rStat.getLastSevenDaysRecord()).sum();
-          String lastSevenDaysRecord = NumberFormat.getIntegerInstance().format(lastSevenDaysRecords);
-          model.addAttribute("lastSevenDaysRecord",lastSevenDaysRecord);
+        /**
+         * last seven days record
+         */
+        int lastSevenDaysRecords = stats.stream().mapToInt(rStat -> rStat.getLastSevenDaysRecord()).sum();
+        String lastSevenDaysRecord = NumberFormat.getIntegerInstance().format(lastSevenDaysRecords);
+        model.addAttribute("lastSevenDaysRecord", lastSevenDaysRecord);
 
-          /**
-           * closed cases
-           */
-          int closedCase = totalRecoveredStat+totalDeathStat;
-          String closedCases = NumberFormat.getIntegerInstance().format(closedCase);
-          int recoveredPercentage = 100-((totalDeathStat*100)/closedCase);
-          int deathPercentage = 100-((totalRecoveredStat*100)/closedCase)-1;
-          model.addAttribute("closedCases",closedCases);
-          model.addAttribute("recoveredPercentage",recoveredPercentage);
-          model.addAttribute("deathPercentage",deathPercentage);
+        /**
+         * closed cases
+         */
+        int closedCase = totalRecoveredStat + totalDeathStat;
+        String closedCases = NumberFormat.getIntegerInstance().format(closedCase);
+        int recoveredPercentage = 100 - ((totalDeathStat * 100) / closedCase);
+        int deathPercentage = 100 - ((totalRecoveredStat * 100) / closedCase) - 1;
+        model.addAttribute("closedCases", closedCases);
+        model.addAttribute("recoveredPercentage", recoveredPercentage);
+        model.addAttribute("deathPercentage", deathPercentage);
 
-          //return value to index.html webPage
+        //return value to index.html webPage
         return "index";
     }
 
     @GetMapping("/map")
-    public String map(){
+    public String map() {
         return "map";
     }
 
