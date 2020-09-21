0830ce3b1fb80753feaeac0817b06e4028cd3bdf
mahbuburriad <mahbubur.riad@northsouth.edu>
2020-09-22 05:59:34 +0600

    google analytics code added

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -35,6 +35,16 @@
 
     @yield('style')
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-178645137-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-178645137-1');
+    </script>
+
 </head>
 <body>
 
