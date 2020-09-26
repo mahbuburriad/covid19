02c6787b2d60e628515023f40b2abfb602dc0c65
mahbuburriad <mahbubur.riad@northsouth.edu>
2020-09-27 00:04:40 +0600

    app service for settings revert for changing server

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,12 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-/*        $basic_settings = Settings::all();
+        $basic_settings = Settings::all();
         unset($settings);
         $settings = array();
         foreach ($basic_settings as $bSet) {
             $settings[$bSet['settings_key']] = $bSet['settings_value'];
         }
-        View::share('settings', $settings);*/
+        View::share('settings', $settings);
     }
 }
