653323154b53d73104a97f8ae86f84387cc04e8b
mahbuburriad <mahbubur.riad@northsouth.edu>
2020-10-01 09:30:26 +0600

    fontawesome cdn change

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -2,7 +2,7 @@
 <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900&amp;display=swap" rel="stylesheet">
 <!-- Font Awesome-->
-<link rel="stylesheet" type="text/css" href="{{asset('assets/css/fontawesome.css')}}">
+<link rel="stylesheet" type="text/css" href="{{url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css')}}">
 <!-- ico-font-->
 <link rel="stylesheet" type="text/css" href="{{asset('assets/css/icofont.css')}}">
 <!-- Themify icon-->
