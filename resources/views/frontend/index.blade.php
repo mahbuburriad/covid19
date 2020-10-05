3bc58b6e59a17b9b6aa005366cf234b92e012727
mahbuburriad <mahbubur.riad@northsouth.edu>
2020-10-05 07:05:13 +0600

    fontawesome link change

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -3,7 +3,7 @@
 @section('title') Coronavirus Update (Live): {{number_format($data[0]->total_cases)}} Cases and {{number_format($data[0]->total_deaths)}} Deaths from @endsection
 
 @section('style')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css"
+    <link rel="stylesheet" href="{{url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css')}}"
           integrity="sha512-q3eWabyZPc1XTCmF+8/LuE1ozpg5xxn7iO89yfSOd5/oKvyqLngoNGsx8jq92Y8eXJ/IRxQbEC+FGSYxtk2oiw==" crossorigin="anonymous"/>
     <link rel="stylesheet" href="{{url('https://cdn.datatables.net/fixedheader/3.1.7/css/fixedHeader.bootstrap4.min.css')}}">
     <link rel="stylesheet" href="{{url('https://cdnjs.cloudflare.com/ajax/libs/jqvmap/1.5.1/jqvmap.min.css')}}">
